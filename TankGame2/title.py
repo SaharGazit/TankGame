a20ff9ceed71206610738ab3f7716d624fb30e7e
cyber <sahargazit1@gmail.com>
2024-02-19 15:24:01 +0200

    reworked title buttons

--- TankGame2/title.py
+++ TankGame2/title.py
@@ -2,9 +2,23 @@ import pygame
 
 
 class Title:
+    BACKGROUND_COLOR = (50, 164, 168)
 
-    @staticmethod
-    def main():
+    def main(self):
+        class Button:
+            FONT = pygame.font.SysFont("None", 50)
+
+            def __init__(self, position, scale, text):
+                self.rect = pygame.Rect(position, scale)
+                self.text = Button.FONT.render(text, False, (0, 0, 0))
+
+            def draw_button(self, screen):
+                pygame.draw.rect(screen, (255, 255, 255), self.rect)
+                screen.blit(self.text, (self.rect.x + 115, self.rect.y + 15))
+
+            def get_rect(self):
+                return self.rect
+
         # initiate program
         pygame.init()
         pygame.display.set_caption('TANK GAME')
@@ -13,12 +27,10 @@ class Title:
         monitor_info = pygame.display.Info()
         screen = pygame.display.set_mode((int(monitor_info.current_w), int(monitor_info.current_h)))
         title_font = pygame.font.Font("resources\\font1.ttf", 90)
-        button_text_font = pygame.font.SysFont("None", 50)
 
         # buttons and texts
         title_text = title_font.render('TANK GAME', False, (0, 0, 0))
-        quit_button_text = button_text_font.render('Quit', False, (0, 0, 0))
-        quit_button = pygame.Rect((100, 800), (300, 70))
+        quit_button = Button((100, 800), (300, 70), 'Quit')
 
         running = True
         while running:
@@ -34,18 +46,28 @@ class Title:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     if event.button == 1:
                         mouse_x, mouse_y = event.pos
-                        if quit_button.collidepoint(mouse_x, mouse_y):
+                        if quit_button.get_rect().collidepoint(mouse_x, mouse_y):
                             running = False
 
+            # background
+            screen.fill(Title.BACKGROUND_COLOR)
 
-            screen.fill((153, 230, 255))
+            # title (TANK GAME)
             screen.blit(title_text, (75, 75))
-            pygame.draw.rect(screen, (255, 255, 255), quit_button)
-            screen.blit(quit_button_text, (215, 815))
+
+            # quit button
+            quit_button.draw_button(screen)
+
             pygame.display.flip()
 
         pygame.quit()
 
 
+
+
+
+
 if __name__ == "__main__":
-    Title.main()
+    pygame.init()
+    title = Title()
+    title.main()
