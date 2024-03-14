0e1ff1509db89615274224d634b356abe942e3be
SaharGazit <sahargazit1@gmail.com>
2024-03-14 21:15:19 +0200

    fixed speed powerup texture

--- TankGame2/scripts/powerup.py
+++ TankGame2/scripts/powerup.py
@@ -7,7 +7,7 @@ class Powerup(Object):
 
     def __init__(self, starting_position, effect):
         # inherited from Object class
-        super().__init__(starting_position, 0, (80, 80), pygame.image.load("../resources/speed.png"))
+        super().__init__(starting_position, 0, (32, 32), pygame.image.load("../resources/speed.png"))
 
         self.effect = effect  # the effect the player would receive as they take the powerup.
         self.duration = Powerup.effects_duration[effect]  # the duration the effect would stay on the player
