86f3b3346548631ef504da097be2e4876487fdd5
SaharGazit <sahargazit1@gmail.com>
2024-04-03 07:58:50 +0300

    client files

--- TankGame2/testing/lobby_server.py
+++ TankGame2/testing/lobby_server.py
@@ -7,7 +7,7 @@ class LobbyServer:
     def __init__(self):
         self.host = '0.0.0.0'  # Server IP
         self.port = 31410  # Server port
-        self.lobby = []
+        self.lobby = {}
         self.MAX_CLIENTS = 8
 
         # tcp socket for handling the lobby stage
@@ -24,7 +24,7 @@ class LobbyServer:
 
         while True:
             # use select to wait for incoming connections or data from existing connections
-            sockets, _, _ = select.select([self.server_socket] + [user.socket for user in self.lobby], [], [])
+            sockets, _, _ = select.select([self.server_socket] + [user.socket for user in self.lobby.keys()], [], [])
 
             for sock in sockets:
                 # check if the new client is already in the lobby or not
@@ -32,45 +32,40 @@ class LobbyServer:
                     # handle new connection
                     conn, addr = self.server_socket.accept()
                     name = conn.recv(1024).decode()
+                    # TODO: later there has to be some kind of authentication here. not everyone who logins here as "Sahar05" is Sahar05.
 
+                    lobby_player_count = len(self.lobby)
                     # check if server is full
-                    if len(self.lobby) == 8:
+                    if lobby_player_count == 8:
                         print("Rejected " + str(addr) + " because server is already full")
                         conn.close()
 
                     else:
                         print(f"Accepted connection from {addr}")
-                        team = (len(self.lobby) % 2) + 1
-                        self.lobby.append(User)
+                        team = (lobby_player_count % 2) + 1
+                        self.lobby[conn] = User(name, lobby_player_count, team)
 
                 else:
                     # get data
                     data = sock.recv(1024)
+                    # identify user
+                    user = self.lobby[sock]
                     if data:
                         data = data.decode()
-                        # TODO: HANDLE IT
+                        print(user.name + " said " + data)
                     else:
                         # kick player if he disconnected
                         sock.close()
-                        self.lobby.remove(sock)
-                        print(f"Connection from {sock.getpeername()} closed")
-
-
-    # def get_user_index(self, addr):
-    #     addr_list = [x.socket_address for x in self.lobby]
-    #     if addr in addr_list:
-    #         return self.lobby.index(addr)
-    #     else:
-    #         return -1
+                        print(f"Connection from {user.name} closed")
 
 
 class User:
-    def __init__(self, sock, username, _id, team):
-        self.socket = sock
-        self.username = username
+    def __init__(self, username, _id, team):  # no socket since it's already a part of a dictionary
+        self.name = username
 
         self.id = _id
         self.team = team
+
 
 
 if __name__ == "__main__":
---
+++ TankGame2/testing/temp_lobby_client.py
@@ -0,0 +1,3 @@
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
